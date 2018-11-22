7f42e7efdfcb73d6ddd4cef8bc5912d84d054908
alexfinastra <alex.perman@gmail.com>
2018-11-22 19:31:37 +0200

    db connexction #6

--- routes/webhook.js
+++ routes/webhook.js
@@ -9,64 +9,9 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res, next) {
   //console.log(JSON.stringify(req, null, 2));
-  if (req.body.result && req.body.result.parameters  ){
-    console.log(" Webhook come wiht : " + JSON.stringify(req.body.result.parameters) )
-  }
-
+  
   res.json({
-    "fulfillmentText": "This is a text response",
-    "fulfillmentMessages": [
-      {
-        "card": {
-          "title": "card title",
-          "subtitle": "card text",
-          "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-          "buttons": [
-            {
-              "text": "button text",
-              "postback": "https://assistant.google.com/"
-            }
-          ]
-        }
-      }
-    ],
-    "source": "example.com",
-    "payload": {
-      "google": {
-        "expectUserResponse": true,
-        "richResponse": {
-          "items": [
-            {
-              "simpleResponse": {
-                "textToSpeech": "this is a simple response"
-              }
-            }
-          ]
-        }
-      },
-      "facebook": {
-        "text": "Hello, Facebook!"
-      },
-      "slack": {
-        "text": "This is a text response for Slack."
-      }
-    },
-    "outputContexts": [
-      {
-        "name": "projects/2/agent/sessions/2/contexts/context name",
-        "lifespanCount": 5,
-        "parameters": {
-          "param": "param value"
-        }
-      }
-    ],
-    "followupEventInput": {
-      "name": "event name",
-      "languageCode": "en-US",
-      "parameters": {
-        "param": "param value"
-      }
-    }
+    "fulfillmentText": "This is a text response" 
   });   
 });
 
