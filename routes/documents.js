c89f57c69b9762252eaf49d7e2238868a96862dc
alexfinastra <alex.perman@gmail.com>
2018-11-22 18:56:35 +0200

    db connexction #3

--- routes/documents.js
+++ routes/documents.js
@@ -14,10 +14,60 @@ router.post('/', function(req, res, next) {
   }
 
   res.json({
-              speech: "Hi",
-              displayText: "Nice to see you :)",
-              source: 'webhook'
-            });   
+    "fulfillmentText": "This is a text response",
+    "fulfillmentMessages": [
+      {
+        "card": {
+          "title": "card title",
+          "subtitle": "card text",
+          "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+          "buttons": [
+            {
+              "text": "button text",
+              "postback": "https://assistant.google.com/"
+            }
+          ]
+        }
+      }
+    ],
+    "source": "example.com",
+    "payload": {
+      "google": {
+        "expectUserResponse": true,
+        "richResponse": {
+          "items": [
+            {
+              "simpleResponse": {
+                "textToSpeech": "this is a simple response"
+              }
+            }
+          ]
+        }
+      },
+      "facebook": {
+        "text": "Hello, Facebook!"
+      },
+      "slack": {
+        "text": "This is a text response for Slack."
+      }
+    },
+    "outputContexts": [
+      {
+        "name": "projects/2/agent/sessions/2/contexts/context name",
+        "lifespanCount": 5,
+        "parameters": {
+          "param": "param value"
+        }
+      }
+    ],
+    "followupEventInput": {
+      "name": "event name",
+      "languageCode": "en-US",
+      "parameters": {
+        "param": "param value"
+      }
+    }
+  });   
 });
 
 
