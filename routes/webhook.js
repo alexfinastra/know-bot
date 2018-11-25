fb8357c47994b3eec7c942274058340bab0d5c2f
alexfinastra <alex.perman@gmail.com>
2018-11-25 13:37:31 +0200

    add zaza #1

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var BUSINESSGUIDES_COLLECTION = "businessguides";
 var USERS_COLLECTION = "users";
 var session_store;
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res) {		
@@ -14,25 +15,9 @@ router.post('/', function(req, res, next) {
   console.log("webhook request :" + JSON.stringify(req.body.queryResult));
   console.log("try :" + JSON.stringify(req.session));
   switch (req.body.queryResult["intent"]["displayName"]){
-    case "Business Guide Trivia":
-      buildQuestion(function(doc){
-        res.json({
-          "fulfillmentText": doc
-        }); 
-      })  
-      break;
-    case "Business Guide - yes":
-      text =  (session_store.correct_answer == "yes") ? "The answer is correct." : "You are missed."
-      res.json({
-        "fulfillmentText": text
-      });
-      break;
-    case "Business Guide - no":
-      text =  (session_store.correct_answer == "yes") ? "The answer is correct." : "You are missed."
-      res.json({
-        "fulfillmentText": text
-      });
-      break;
+    case "New Office":
+      sendScriptsByMail(req.body.queryResult.parameters, res)
+      break;    
     case "GetEmail":
       session_store.email = req.body.queryResult.parameters.email;
       getUser(session_store["email"], function(user){
@@ -84,6 +69,37 @@ function getUser(email, cb){
       cb(doc);
     } else {
       return null;
+    }
+  });
+}
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'finastra.integration.team@gmail.com',
+    pass: 'koza2012'
+  }
+});
+
+function sendScriptsByMail(opts, res){
+  var mailOptions = {
+    from: 'finastra.integration.team@gmail.com',
+    to: "zaza.tsivion@finastra.com",
+    subject: 'New Office - script',
+    text: "INSERT INTO BANKS (OFFICE,UPDATE_DATE,TIME,OPERATOR,OFFC_NAME,OFFC_ADDR1,OFFC_ADDR2,BSNESSDATE,REL_ORDER,FPRINT,TIME_STAMP,CURRENCY,COUNTRYCODE,CALNAME,ACCOUNTNO,MIN_ACC_LENGTH,MAX_ACC_LENGTH,HEDGE_CURRENCY,CUST_CODE,REC_STATUS,LEGAL_ENTITY,MIN_FEE_TRANS_AMOUNT,FEE_CURRENCY,PROFILE_CHANGE_STATUS,PENDING_ACTION,PROCESSING_STATUS,EFFECTIVE_DATE,REFERENCEBRANCH,FORCE_LEAD_ZERO,TRUNC_LEAD_ZERO,OTHER_BASE_CCY,NEXT_BSNESSDATE,PREV_BSNESSDATE,UID_BANKS,CUTOFF_NAME,HISTORY_PROC_DT,LANG,DEF_BOOKNG_ENT,DEF_CNSLD_FEE_POST,DEF_CNSLD_PNL_POST,POST_ZERO_FEE,BTCH_CTRL_ID,DEF_CNSLD_TAX_POST,DEF_CNSLD_TAX_PNL_POST,ISO_CODE_LOGICAL_FIELD,IN_SCOPE_IND,FILE_DUPLICATE_CHECK_IND,MIN_NUM_OF_AUTHORIZ_SUBSCRIBER,LIMIT_CCY,RFF_DUPLICATE_CHECK_IND,ESTM_DUPLICATE_CHECK_IND,ORDER_TYPE,FORMAT_NAME,STATEMENT_DUPLICATE_CHECK_IND,STATEMENT_FORMAT_NAME) VALUES ('"+ opts["NewOfficeName"] +"',null,null,null,'"+ opts["NewOfficeName"] +" Office',null,null,to_date('14-NOV-16','DD-MON-RR'),null,null,'2016-11-14 09:36:00.666','"+ opts["CaseCurrency"] +"','"+ opts["OfficeCountry"] +"','"+ opts["CaseCurrency"] +"',null,'9 ','34',null,'--BIC--','AC',null,null,null,'NO','UP',null,to_date('14-NOV-16','DD-MON-RR'),null,0,0,null,to_date('15-NOV-16','DD-MON-RR'),to_date('14-NOV-16','DD-MON-RR'),'"+ opts["NewOfficeName"] +"',null,to_date('11-NOV-16','DD-MON-RR'),'ENGLISH',null,null,null,0,null,null,null,null,1,1,null,'"+ opts["CaseCurrency"] +"',null,null,null,null,null,null)"
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+       res.json({
+          "fulfillmentText": "Sorry , but the supplied data is incorrect please repat the procedure."
+        }); 
+    } else {
+      console.log('Email sent: ' + info.response);
+       res.json({
+          "fulfillmentText": "Office script was successfully created and send to your email. :)"
+        }); 
     }
   });
 }
